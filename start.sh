d8113fc6e6ab7806331189adb8935b74709d4237
Robert Brown <robert.brown@contentcoffee.com>
2017-10-31 14:45:29 +0100

    better start script.

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 curl -sS https://codeload.github.com/ContentCoffee/DrupalBase/zip/master > file.zip 
-unzip file.zip 
+bsdtar -xf file.zip -s'|[^/]*/||'
+rm -rf DrupalBase-master
 rm file.zip
 composer install
 rm start.sh
