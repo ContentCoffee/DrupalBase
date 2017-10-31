d77856d0fa45845be7b25f1a6910e45f8a3ae244
Robert Brown <robert.brown@contentcoffee.com>
2017-10-31 14:37:13 +0100

    spelling.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -sS ihttps://codeload.github.com/ContentCoffee/DrupalBase/zip/master > file.zip 
+curl -sS https://codeload.github.com/ContentCoffee/DrupalBase/zip/master > file.zip 
 unzip file.zip 
 rm file.zip
 composer install
